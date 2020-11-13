0916e0612dd8d1aa0bd9cb48e616766402133ece
huangxinpeng <gp.3098@qq.com>
2020-11-14 01:06:11 +0800

    🎉

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container">
+      <h1>
+        欢迎使用 Huang 待办事项！
+      </h1>
+      <div class="input-add">
+        <input type="text" name="todo" id="todo" />
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+      <div class="filters">
+        <span class="filter active">全部</span>
+        <span class="filter">已完成</span>
+        <span class="filter">未完成</span>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item">
+          <label for="">
+            <input type="checkbox" name="" id="" />
+            Todo 1
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label for="">
+            <input type="checkbox" name="" id="" />
+            Todo 2
+            <span class="check-button"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
